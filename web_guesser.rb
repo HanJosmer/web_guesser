337ac7c401ed8417f7ca72ab6c595c912fd44525
John Hosmer <john.n.hosmer@gmail.com>
2020-03-06 18:18:21 -0800

    I4: add guess count, lose page, and cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,23 +6,53 @@ set :number, rand(101)
 
 def check_guess guess, number
     if guess.to_i.between?(number + 1, number + 5)
-        return "Too high!"
+        return 3
     elsif guess.to_i.between?(number - 5, number - 1)
-        return "Too low!"
+        return 1
     elsif guess.to_i == number
-        return "You got it right!!"
+        return 2
     elsif guess.to_i > number + 5
-        return "Way too high!"
+        return 4
     elsif guess.to_i < number - 5
-        return "Way too low!"
+        return 0
     else
-        return ""
+        return nil
     end
 end
 
+@@guesses = 6
+
+messages = [
+    "Way too low!",
+    "Too low!",
+    "You got it right!!",
+    "Too high!",
+    "Way too high!"
+]
+
+colors = [
+    "red",
+    "pink",
+    "green",
+    "pink",
+    "red"
+]
+
 get '/' do
     guess = params["guess"]
-    message = check_guess(guess, settings.number)
+    cheat = params["cheat"]
+    accuracy = check_guess(guess, settings.number)
+    message = messages[accuracy]
+    color = colors[accuracy]
+    @@guesses = @@guesses - 1
     
-    erb :index, :locals => {:number => settings.number, :message => message}
+    if @@guesses < 1
+        erb :loser
+    else
+        erb :index, :locals => {:number => settings.number,
+                                :message => message,
+                                :bg_color => color,
+                                :guesses => @@guesses,
+                                :cheat_mode => cheat}
+    end
 end
